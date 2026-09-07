b4b275d88393edb98023415b35d2d3e1cdfb2887
ousama lasri <oussama.lasri.2k@gmail.com>
2026-09-07 18:38:57 +0100

    feat: add exception handlers in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,23 @@ from app.ai.rag.chain import get_documents_from_retriever
 from app.api.v1.router import api_router
 from app.utils.init_db import create_tables
 from app.core.middleware import AuthMiddleware
+from app.core.exceptions import (
+    DocumentError,
+    DocumentLoadError,
+    DocumentProcessingError,
+    DocumentRetrievalError,
+    DocumentStorageError,
+    UnsupportedFileTypeError,
+)
+
+from app.core.exception_handlers import (
+    document_error_handler,
+    document_load_handler,
+    document_processing_handler,
+    document_retrieval_handler,
+    document_storage_handler,
+    unsupported_file_type_handler,
+)
 
 app = FastAPI()
 app.add_middleware(AuthMiddleware)
@@ -17,3 +34,33 @@ async def root():
 
 
 app.include_router(api_router, prefix="/api/v1")
+
+app.add_exception_handler(
+    UnsupportedFileTypeError,
+    unsupported_file_type_handler,
+)
+
+app.add_exception_handler(
+    DocumentLoadError,
+    document_load_handler,
+)
+
+app.add_exception_handler(
+    DocumentStorageError,
+    document_storage_handler,
+)
+
+app.add_exception_handler(
+    DocumentRetrievalError,
+    document_retrieval_handler,
+)
+
+app.add_exception_handler(
+    DocumentProcessingError,
+    document_processing_handler,
+)
+
+app.add_exception_handler(
+    DocumentError,
+    document_error_handler,
+)
